f1b8291d31414b2f1c86478c5bb41b501f10bbdb
ilmukov <30494462+ilmukov@users.noreply.github.com>
2021-07-28 20:21:59 +0300

    Initial Commit

---
+++ SwiftUI Api Call/SwiftUI_Api_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Api_CallApp.swift
+//  SwiftUI Api Call
+//
+//  Created by Stepan Ilmukov on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Api_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
